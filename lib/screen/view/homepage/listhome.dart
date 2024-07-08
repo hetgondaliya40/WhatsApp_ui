dd21be4dd47d8c76f5d19d5d12846ed298a3c0cf
hetgondaliya <132036737+hetgondaliya40@users.noreply.github.com>
2024-07-08 11:06:58 +0530

    Update listhome.dart

--- lib/screen/view/homepage/listhome.dart
+++ lib/screen/view/homepage/listhome.dart
@@ -8,67 +8,67 @@ List whatsapp = [
     "time": "12 : 09",
   },
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/58608213?s=80&v=4",
     "name": "Taksh Malaviya",
     "sub": "flutter devlopar",
     "time": "01 : 09",
   },
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/150034575?v=4",
     "name": "Dhruv Kathiriya",
     "sub": "Android devlopar",
     "time": "11 : 23",
   },
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/150029430?s=100&v=4",
     "name": "Prince Buha",
     "sub": "Busy ",
     "time": "10 : 23",
   },
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/36331702?s=80&v=4",
     "name": "Nirav",
     "sub": "Urgant call only",
     "time": "03 : 23",
   },
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/9635493?s=80&v=4",
     "name": "Prince ladumor",
     "sub": "Jay Shree Ram",
     "time": "11 : 23",
   },
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/8232573?s=80&v=4",
     "name": "Vraj kathiriya",
     "sub": "At work",
     "time": "06 : 23",
   },
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/534678?s=80&v=4",
     "name": "Mahil",
     "sub": "Don't Disturb",
     "time": "07 : 23",
   },
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/33294549?s=80&v=4",
     "name": "Aditya",
     "sub": "Hello",
     "time": "04 : 23",
   },
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/42710380?s=80&v=4",
     "name": "Pruthviraj",
     "sub": "How are  you",
     "time": "07 : 23",
   },
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/14161798?s=80&v=4",
     "name": "Meet",
     "sub": "Android devlopar",
     "time": "10 : 23",
   },
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/9635493?s=80&v=4",
     "name": "Raj",
     "sub": "Android devlopar",
     "time": "11 : 22",
@@ -82,22 +82,22 @@ List whatsappstatus = [
     "sub": " 20 min ago",
   },
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/42710380?s=80&v=4",
     "name": "Pruthviraj",
     "sub": "1 hour ago",
   },
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/150034575?v=4",
     "name": "Dhruv Kathiriya",
     "sub": "1:30 hour ago",
   },
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/58608213?s=80&v=4",
     "name": "Taksh Malaviya",
     "sub": "2:12 hour ago",
   },
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/36331702?s=80&v=4",
     "name": "Viraj Kathiriya",
     "sub": "3 hour ago",
   },
@@ -105,7 +105,7 @@ List whatsappstatus = [
 
 List calls = [
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/36331702?s=80&v=4",
     "name": "Raj",
     "sub": "6 , jun 12 : 36",
     "icon2": Icon(
@@ -120,7 +120,7 @@ List calls = [
     ),
   },
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/58608213?s=80&v=4",
     "name": "Taksh Malaviya",
     "sub": "7 , jun 1 : 22",
     "icon2": Icon(
@@ -135,7 +135,7 @@ List calls = [
     ),
   },
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/150034575?v=4",
     "name": "Dhruv Kathiriya",
     "sub": "10 , jun 1 : 01",
     "icon2": Icon(
@@ -150,7 +150,7 @@ List calls = [
     ),
   },
   {
-    "img": "https://avatars.githubusercontent.com/u/132036737?v=4",
+    "img": "https://avatars.githubusercontent.com/u/42710380?s=80&v=4",
     "name": "Pruthviraj",
     "sub": "22 , jun 6 : 20",
     "icon": Icon(
